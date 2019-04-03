6a178d1040d049d0b5b5cd75ab26bb9e58bf4b94
qee-wong <qee_wong@163.com>
2019-04-03 13:49:07 +0800

    修改后台请求地址

--- config/index.js
+++ config/index.js
@@ -12,7 +12,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    //host: 'localhost', // can be overwritten by process.env.HOST
+    host: '193.169.100.166', // can be overwritten by process.env.HOST
     port: 8013, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
